3c80ad55ec97e1dc3d79cca3f26c111782527d39
_Ricky <riccardo.cortelazzo@hotmail.it>
2015-12-19 14:37:06 +0100

    Base for Time Attack Mode

---
+++ Assets/Scripts/Gamelevel/CustomSlider.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Game
+{
+    public class CustomSlider : MonoBehaviour
+    {
+        public Slider slider;
+        public Text HoverText;
+
+        //update slider hovertext
+        public void UpdateText(string text)
+        {
+            HoverText.text = text;
+        }
+
+        //set the slider value
+        public void SetSliderPos(float newPos)
+        {
+            slider.value = newPos;
+        }
+
+        //add val to slider value
+        public void AddToSliderPos(float val)
+        {
+            slider.value += val;
+        }
+
+        public void SetSliderMaxValue(float val)
+        {
+            slider.maxValue = val;
+        }
+
+        //fill the bar and set the opposite fill direction
+        public void Inverse()
+        {
+            slider.value = slider.maxValue;
+            slider.direction = Slider.Direction.RightToLeft;
+        }
+
+        public void InvereAndSet(float maxVal)
+        {
+            SetSliderMaxValue(maxVal);
+            Inverse();
+        }
+
+    }
+}
--- Assets/Scripts/Gamelevel/GameManager.cs
+++ Assets/Scripts/Gamelevel/GameManager.cs
@@ -7,9 +7,18 @@ namespace Game
 {
     public class GameManager : MonoBehaviour {
 
+        public enum GameModes {Time_Attack, Rush }
+
+        public GameModes GameMode;
+
         bool isStarted = true;
 
+
         [SerializeField] int score = 0;
+        const int TimeAttack_MaxScore = 25;
+        int PointsToAdd = 0;
+
+        [SerializeField] float timer = 0f;
 
         [SerializeField] Text HeaderText; // text displayed on top 
         [SerializeField] Text ScoreText;
@@ -17,10 +26,16 @@ namespace Game
 
         [SerializeField] List<GameButton> GameButtons; // list of 9 buttons references
 
+        [SerializeField] CustomSlider bar;
+
         ExtendedColor[] colors = new ExtendedColor[]
             { ColorExtension.red,ColorExtension.blue,ColorExtension.cyan,ColorExtension.gray,ColorExtension.green,
                 ColorExtension.black,ColorExtension.white,ColorExtension.yellow
             }; // all color list
+
+        delegate void BaseAction(); 
+
+        BaseAction UpdateAction;//function to rely with different timer uses
 
         EventMaster ev;
 
@@ -29,17 +44,45 @@ namespace Game
             ev = References.evMaster;
             ev.OnRightButtonClick += NewButtons; //TODO: versione temporanea 
             ev.OnRightButtonClick += addScore;
+            
         }
 
         void OnDisable()
         {
             ev.OnRightButtonClick -= NewButtons;
             ev.OnRightButtonClick -= addScore;
+            //ev.OnWrongButtonClick -= TimeAttack_WrongClick;
+            switch (GameMode)
+            {
+                case GameModes.Time_Attack:
+                    UpdateAction -= TimeAttack_TimerAction; // adds timer function  to update delegate
+                    ev.OnRightButtonClick -= TimeAttack_UpdateBarPos; // update the bar fill
+                    ev.OnWrongButtonClick -= TimeAttack_WrongClick; // sets the action for wrong click
+                    break;            
+            }
+        }
+
+        void Init()
+        {
+            switch (GameMode)
+            {
+                case GameModes.Time_Attack:
+                    PointsToAdd = 1;
+                    UpdateAction += TimeAttack_TimerAction; // adds timer function  to update delegate
+                    ev.OnRightButtonClick += TimeAttack_UpdateBarPos; // update the bar fill
+                    ev.OnWrongButtonClick += TimeAttack_WrongClick; // sets the action for wrong click
+                    bar.SetSliderMaxValue(TimeAttack_MaxScore);
+                    break;
+                default:
+                    Debug.LogError("No implementation for that GameMode");
+                    break;
+            }
         }
 
         // Use this for initialization
         void Start() {
 
+            Init();
             if (isStarted)
             {
                 SetUpButtonsColor(Random.Range(0,GameButtons.Count), Random.Range(0,colors.Length));
@@ -86,11 +129,43 @@ namespace Game
 
         public void addScore()
         {
-            //TODO:calcolare punteggio in base a tempo
-            int points = 10;
-            ScoreAdded.text = "+" + points;
-            score += points;
+            //TODO:calcolare punteggio in base a tempo       
+            ScoreAdded.text = "+" + PointsToAdd;
+            score += PointsToAdd;
             ScoreText.text = "Score: " + score;
         }
+
+
+        void TimeAttack_TimerAction()
+        {
+            timer += Time.deltaTime;
+            TimeAttack_UpdateBarTimer();
+        }
+
+        void TimeAttack_WrongClick()
+        {
+            timer += 10f;
+        }
+
+        void TimeAttack_UpdateBarPos()
+        {
+            bar.SetSliderPos(score);
+        }
+
+        void TimeAttack_UpdateBarTimer()
+        {
+            System.TimeSpan time = System.TimeSpan.FromSeconds(timer);
+            string timeString = string.Format("{0:D}:{1:D2}:{2:D1}", time.Minutes, time.Seconds, Mathf.RoundToInt(time.Milliseconds/100f));
+            bar.UpdateText(timeString);
+        }
+
+
+        void Update()
+        {
+            if(UpdateAction != null)
+            UpdateAction();
+        }
+
     }
+
 }
